214cfa1f71e10a57020d7b95aa80733ac9c0b45a
NomuraShunsuke <37791696+NomuraShunsuke@users.noreply.github.com>
2020-03-07 17:59:11 +0900

    feat:コンパイルエラーの解消

---
+++ pages/result.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout column>
+    <result-chart/>
+    <result-list/>
+    <v-btn @click="back">もう一度診断する</v-btn>
+  </v-layout>
+</template>
+
+<script lang="ts">
+
+import {Vue, Component} from 'nuxt-property-decorator'
+// デコレータ内で、このコンポーネントを
+// 使うことを宣言し、同時にimport
+
+@Component({
+  components: {
+    ResultChart: () =>import('~/components/ResultChart.vue'),
+    ResultList: () => import('~/components/ResultList.vue')
+  }})
+  export default class Result extends Vue { }
+</script>
